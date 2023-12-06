32d9574ef81fe81f0c67fbcf38ee6df6429e0e13
C:\Users\idkbreh <93085924+idkbreh@users.noreply.github.com>
2023-12-06 23:11:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine','ejs')
 app.set('views', __dirname + '/views');
 app.use(flash())
